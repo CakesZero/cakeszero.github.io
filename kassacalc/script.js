412264afbf0665213d9f8ca4af0655b53cebb847
CakesZero <67364634+CakesZero@users.noreply.github.com>
2025-03-17 15:00:24 +0500

    Add files via upload

---
+++ kassacalc/script.js
@@ -0,0 +1,135 @@
+const s = id => document.getElementById(id)
+
+const kassa = s('kassa')
+const beznal = s('beznal')
+const nal = s('nal')
+const dolg = s('dolg')
+const bankBeznal = s('bank-beznal')
+const bankNal = s('bank-nal')
+const procentBtn = s('procent-btn')
+const saveBtn = s('save-btn')
+const beznalBtn = s('beznal-btn')
+const nalBtn = s('nal-btn')
+const updateBtn = s('update-btn')
+const clearBtn = s('clear-btn')
+const result = s('result')
+const dataScreen = s('data-screen')
+
+let currentData = {}
+
+clearBtn.addEventListener('click', () => localStorage.setItem('data', '[]'))
+procentBtn.addEventListener('click', () => procentBtn.value == '50%'?procentBtn.value = '40%':procentBtn.value = '50%')
+saveBtn.addEventListener('click', saveData)
+beznalBtn.addEventListener('click', beznalUpdate)
+nalBtn.addEventListener('click', nalUpdate)
+updateBtn.addEventListener('click', update)
+
+function beznalUpdate() { beznal.value = Number(kassa.value) - Number(nal.value) }
+function nalUpdate() { nal.value = Number(kassa.value) - Number(beznal.value) }
+
+function saveData() {
+    let data = JSON.parse(localStorage.getItem('data')) || []
+    data.push(currentData)
+    localStorage.setItem('data', JSON.stringify(data))
+    printData()
+}
+
+function printData() {
+    const data = JSON.parse(localStorage.getItem('data'))
+    if (!data || data.length == 0) return
+    const dataText = []
+    for (const obj of data) {
+        const k = obj.kassa
+        const bn = obj.beznal
+        const n = obj.nal
+        const d1 = obj.dolg
+        const bn1 = obj.bankBeznal
+        const n1 = obj.bankNal
+        const zp = obj.zp
+        const timestamp = obj.timestamp
+        const date = new Date(timestamp)
+        
+        let fkassa = (bn&&n)?` (${bn}бн+${n}н)`:((bn==k)&&!n)?`бн`:(!bn&&(n==k))?`н`:''
+        let fdolg = d1?` (${d1}долг)`:''
+        let fbank = (bn1&&n1)?`${bn1}бн+${n1}н`:(bn1&&!n1)?`${bn1}бн`:(!bn1&&n1)?`${n1}н`:''
+        
+        dataText.push([
+            `${date.getDate().toString().padStart(2, 0)}.${date.getMonth().toString().padStart(2, 0)}`,
+            `Касса: ${k}${fkassa}`,
+            `Зп: ${zp}${fdolg}`,
+            `Банк: ${fbank}`
+        ].join('\n'))
+    }
+    dataScreen.textContent = dataText.join('\n\n')
+}
+
+function update() {
+    const k = Number(kassa.value)
+    const bn = Number(beznal.value)
+    const n = Number(nal.value)
+    const d = Number(dolg.value)
+    const Bbn = Number(bankBeznal.value)
+    const Bn = Number(bankNal.value)
+    const procent = procentBtn.value.slice(0, 2)/100
+
+    let zp = Math.floor(k*procent/50)*50
+    let bn1 = bn + Bbn
+    let n1 = n + Bn
+    let d1 = d
+
+    if (zp > n1) {
+        // Долг увеличивается
+        d1 += zp - n1
+        bn1 -= zp - n1
+        n1 = 0
+    } else {
+        // Долг уменьшается или остается без изменений
+        n1 -= zp
+        if (d1 > n1) {
+            d1 -= n1
+            n1 = 0
+        } else d1 = 0
+    }
+
+    let fkassa = (bn&&n)?` (${bn}бн+${n}н)`:((bn==k)&&!n)?`бн`:(!bn&&(n==k))?`н`:''
+    let fdolg = d1?` (${d1}долг)`:''
+    let fbank = (bn1&&n1)?`${bn1}бн+${n1}н`:(bn1&&!n1)?`${bn1}бн`:(!bn1&&n1)?`${n1}н`:''
+
+    currentData = {
+        kassa: k,
+        beznal: bn,
+        nal: n,
+        dolg: d1,
+        bankBeznal: bn1,
+        bankNal: n1,
+        zp: zp,
+        timestamp: Date.now()
+    }
+
+    result.textContent = [
+        `Касса: ${k}${fkassa}`,
+        `Зп: ${zp}${fdolg}`,
+        `Банк: ${fbank}`
+    ].join('\n')
+
+    beznalUpdate(),
+    nalUpdate()
+}
+
+// ===TEST===
+function test() {
+    kassa.value = 7400
+    nal.value = 2050
+    bankBeznal.value = 18700
+    bankNal.value = 0
+    dolg.value = 0
+    update()
+}
+// ===TEST===
+
+window.onload = () => {
+    // setInterval(update, 100)
+    update()
+    printData()
+    test()
+}
